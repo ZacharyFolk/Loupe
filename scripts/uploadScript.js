675352d2013951c7359db97b9866f615e417b919
Zachary Folk <zacharyfolk@gmail.com>
2012-01-07 22:27:24 -0800

    clean up

--- scripts/uploadScript.js
+++ scripts/uploadScript.js
@@ -4,8 +4,7 @@ jQuery('#upload_image_button').click(function() {
 		window.send_to_editor = function(html) {
 		 imgurl = jQuery('img',html).attr('src');
 		 jQuery('#single_photo').val(imgurl);
-		 tb_remove();
-		
+		 tb_remove();		
 		}
 
 	 tb_show('', 'media-upload.php?type=image&amp;TB_iframe=true');
