b187506b6d9589db77cf8116fe58ce0208ad28e9
Zachary Folk <zacharyfolk@gmail.com>
2012-01-07 21:50:20 -0800

    moved wp_title to functions

--- header.php
+++ header.php
@@ -2,28 +2,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
-<title>
-<?php
-	/*
-	 * Print the <title> tag based on what is being viewed.
-	 */
-	global $page, $paged;
-
-	wp_title( '|', true, 'right' );
-
-	// Add the blog name.
-	bloginfo( 'name' );
-
-	// Add the blog description for the home/front page.
-	$site_description = get_bloginfo( 'description', 'display' );
-	if ( $site_description && ( is_home() || is_front_page() ) )
-		echo " | $site_description";
-
-	// Add a page number if necessary:
-	if ( $paged >= 2 || $page >= 2 )
-		echo ' | ' . sprintf( __( 'Page %s', 'twentyten' ), max( $paged, $page ) );
-
-	?></title>
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
@@ -127,14 +106,14 @@
 		<?php 
 	if (is_single()){ ?>
 	
-<ul id="controls">
-	<li class="zoom">Zoom : </li>
-	<li class="zoom_out"><a id="out" href="#"> - </a></li>
-	<li class="zoom_in"><a id="in" href="#"> + </a></li>
-	<li>|</li>
-    <li id="infoLink">info <span class="close"></span></li>
-  <!--    <a id="fit" href="#">100%</a>
- <a id="orig" href="#">orig</a> -->
+	<ul id="controls">
+		<li class="zoom">Zoom : </li>
+		<li class="zoom_out"><a id="out" href="#"> - </a></li>
+		<li class="zoom_in"><a id="in" href="#"> + </a></li>
+		<li>|</li>
+	    <li id="infoLink">info <span class="close"></span></li>
+	  <!--    <a id="fit" href="#">100%</a>
+	 <a id="orig" href="#">orig</a> -->
 	</ul>
     <?php } ?>
 	<div id="tagList">
